81a5e10b255eb11caf00bf68e34fe0c3a3e92666
PushpakB3096 <rtpushpak@gmail.com>
2021-05-22 16:52:36 +0530

    Add styling to left nav

--- client/src/components/LeftNav.tsx
+++ client/src/components/LeftNav.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import WoofLogo from '../styles/assets/woof-logo.png'
+import "../styles/leftNav.css"
 
 const LeftNav = () => {
     return (
         <div>
-            LeftNav
+            {/* Woof logo goes here */}
+            <Link to="/users">
+                <img src={WoofLogo} alt="logo" style={{ width: "40px" }} />
+            </Link>
+            <Link to="/users">
+                <h2>
+                    <i className="fa fa-home" aria-hidden="true"></i>
+                    <span className="title">Home</span>
+                </h2>
+            </Link>
         </div>
     )
 }
